34079052f21be830ddbfd622da107dd216e9c63e
David <drdavialberto5@gmail.com>
2023-10-19 12:55:20 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,5 +1,6 @@
 package mx.uv;
 
+import static spark.Spark.*;
 /**
  * Hello world!
  *
@@ -9,5 +10,21 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/", 
+        (request,response)->"<h1>Hola Mundo</h1>"
+        );
+
+        get("/1", 
+        (request,response)->"<h1>Adiós Mundo</h1>"
+        );
+        
+        get("/2", 
+        (request,response)->"<h1>Que tal Mundo</h1>"
+        );
+        
+        get("/3", 
+        (request,response)->"<h1>Ok Mundo</h1>"
+        );
+        
     }
 }
